5ac5d9237c149a72f0a4097807b934ab42875e56
ltseng3 <ltseng3@illinois.edu>
2022-01-09 22:29:22 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,3 +5,4 @@ tail -n +200 latFileRead-2.txt > tmp.txt && mv tmp.txt latFileRead-2.txt
 tail -n +200 latFileWrite-0.txt > tmp.txt && mv tmp.txt latFileWrite-0.txt
 tail -n +200 latFileWrite-1.txt > tmp.txt && mv tmp.txt latFileWrite-1.txt
 tail -n +200 latFileWrite-2.txt > tmp.txt && mv tmp.txt latFileWrite-2.txt
+python3.8 client_metrics.py
